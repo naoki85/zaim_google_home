8b7378f25eaefe5edba434420d6a2dbc8196a4aa
naoki85 <naoki.yoneyama.85@gmail.com>
2018-01-03 16:45:51 +0900

    Return error unless params

--- app/controllers/api/zaim_api_controller.rb
+++ app/controllers/api/zaim_api_controller.rb
@@ -1,17 +1,28 @@
 class Api::ZaimApiController < ApiApplicationController
   def index
-    user = User.last
-    access_token = MyOauth.new.set_access_token(user.access_token, user.access_token_secret)
-    zaim_api = ZaimApi.new(access_token)
-    options = { start_date: Date.yesterday.strftime('%Y-%m-%d'), end_date: Date.today.strftime('%Y-%m-%d') }
-    results = zaim_api.home_money(options)
-    # results = Rails.cache.fetch("zaim_api/home/money/#{user.id}", expired_in: 1.hour) do
-    #   zaim_api.home_money(options)
-    # end
-    sum = 0
-    results['money'].each do |money|
-      sum += money['amount'].to_i
+    if invalid_params?
+      @message = 'ログインに失敗しました'
+    else
+      user = User.find_by(uid: params[:uid].downcase, password: params[:password])
+      if user
+        access_token = MyOauth.new.set_access_token(user.access_token, user.access_token_secret)
+        zaim_api = ZaimApi.new(access_token)
+        options = { start_date: Date.yesterday.strftime('%Y-%m-%d'), end_date: Date.today.strftime('%Y-%m-%d') }
+        results = zaim_api.home_money(options)
+        sum = 0
+        results['money'].each do |money|
+          sum += money['amount'].to_i
+        end
+        @message = "昨日は#{sum}円使いました"
+      else
+        @message = 'ユーザーが見つかりませんでした'
+      end
     end
-    @message = "昨日は#{sum}円使いました"
+  end
+
+  private
+
+  def invalid_params?
+    !params[:uid] || !params[:password]
   end
 end
