ad9122cd1437838625e4d35235726a41e6413e61
naoki85 <naoki.yoneyama.85@gmail.com>
2018-01-03 17:13:28 +0900

    Change saving uid in Hiragana

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,9 @@
 class User < ApplicationRecord
   # TODO: 保存する前にハッシュ化する
   # include Encryptor
-  VALID_UID_REGEX = /\A[a-zA-Z]+\z/
+  VALID_UID_REGEX = /\A\p{Hiragana}+\z/
   VALID_PASSWORD_REGEX = /\A[0-9]+\z/
 
   validates :uid, format: { with: VALID_UID_REGEX }, uniqueness: { case_sensitive: false }, allow_nil: true
   validates :password, format: { with: VALID_PASSWORD_REGEX }, allow_nil: true
-  before_save :uid_downcase
-
-  private
-
-  def uid_downcase
-    unless uid.nil?
-      uid.downcase!
-    else
-      true
-    end
-  end
 end
